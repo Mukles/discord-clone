74753d47339c94b33d92694cdd4c83c0eef086ac
Mukles <Mukleshossen1763@gmail.com>
2023-02-09 13:35:33 +0600

    layout done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -14,110 +9,63 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
+      <main>
+        <div className="flex min-h-screen text-white">
+          <div className="bg-gray-800 p-4">
+            <div className="bg-white text-gray-800 w-12 h-12 rounded-full flex items-center justify-center">
+              TW
+            </div>
+          </div>
+          <div className="bg-gray-700 w-60 flex flex-col">
+            <div className="p-4 shadow-md">Tailwind CSS</div>
+            <div className="p-4 flex-1">channels</div>
+          </div>
+
+          <div className="bg-gray-600 flex-1 flex flex-col">
+            <div className="p-4 shadow-md">general</div>
+            <div className="p-4 flex-1">messages</div>
+          </div>
+        </div>
+        {/* <div className="bg-gray-700 text-white min-h-screen flex items-center justify-center">
+          <div className="max-w-lg">
+            <div className="flex hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+              <img
+                className="w-10 h-10 rounded-full mr-4"
+                src="/adamwathan.jpeg"
+                alt=""
               />
-            </a>
+              <div>
+                <p className="flex items-baseline">
+                  <span className="text-green-500 mr-2 text-sm font-medium">
+                    adamwathan
+                  </span>
+                  <span className="text-xs text-gray-500">01/15/2021</span>
+                </p>
+                <p className="text-gray-300">
+                  You should never use something like leading relaxed with a big
+                  font size, it goes against all typography best practices. Line
+                  height should decrease as font size gets bigger
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-1 hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+              <p className="text-gray-300 pl-14">
+                You can override it in your config if you want but ultimately we
+                chose the defaults they did because they let you get results
+                closest to what a professional designer would do more easily
+              </p>
+            </div>
+
+            <div className="mt-1 hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+              <p className="text-gray-300 pl-14">
+                Since we changed this in tailwind 2 I’ve almost never used a
+                leading class at all
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        </div> */}
       </main>
     </>
-  )
+  );
 }
